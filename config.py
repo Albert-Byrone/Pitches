883d5bb5dea4ac7a2d6c53835b16695d77f4e8df
Albert Byrone <albertbyrone1677@gmail.com>
2019-10-22 11:13:44 +0300

    add app configuration classes

---
+++ config.py
@@ -0,0 +1,22 @@
+class Config:
+
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://byrone:Albert254@localhost/pitcher'
+    SECRET_KEY = 'albertbyrone'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    MAIL_SERVER = 'stmp.googleemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+
+class ProdConfig(Config):
+    pass
+
+class DevConfig(Config):
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
