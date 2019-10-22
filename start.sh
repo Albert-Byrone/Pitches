60535a1699984e575d0807659c8cc06268826d89
Albert Byrone <albertbyrone1677@gmail.com>
2019-10-22 11:54:50 +0300

    add a start.sh file

---
+++ start.sh
@@ -0,0 +1,6 @@
+SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://byrone:Albert254@localhost/pitcher'
+SECRET_KEY='albertbyrone'
+export MAIL_USERNAME='albertbyrone1677@gmail.com'
+export MAIL_PASSWORD='albertbyrone00'
+
+python3.6 manage.py server
