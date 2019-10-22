81f86229613619ffe421ad05cca53d8dc8d9cd12
Albert Byrone <albertbyrone1677@gmail.com>
2019-10-22 09:22:04 +0300

    upvote function that saves the users likes

--- app/models.py
+++ app/models.py
@@ -79,3 +79,12 @@ class Comment(db.Model):
 
     def __repr__(self):
         return f"comment:{self.comment}"
+
+class Upvote(db.Model):
+    __tablename__ = 'upvotes'
+
+    id = db.Column(db.Integer,primary_key=True)
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
+    pitch_id = db.Column(db.Integer,db.ForeignKey('pitches.id'))
+
+
