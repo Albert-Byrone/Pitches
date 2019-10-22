511ced7288b6f41f624a2cac8400633d788cf645
Albert Byrone <albertbyrone1677@gmail.com>
2019-10-22 11:55:39 +0300

    minor changes

--- app/auth/views.py
+++ app/auth/views.py
@@ -5,6 +5,7 @@ from .form import LoginForm,RegisterForm
 from ..models import User
 from flask_login import login_user
 from ..email import mail_message
+from .. import db
 
 @auth.route('/login',methods=['GET','POST'])
 def login():
@@ -15,7 +16,6 @@ def login():
             login_user(user,form.remember.data)
             return redirect(request.args.get('next') or url_for('main.index'))
         flash('Invalid username and password')
-
     return render_template('auth/login.html',loginform=form)
 
 @auth.route('/signup',methods = ['GET','POST'])
@@ -24,7 +24,7 @@ def signup():
     if form.validate_on_submit():
         user = User(email=form.email.data,username = form.username.data,password=form.password.data)
         user.save_user()
-        mail_message("Welcome to Pitch-World","email/welcome_user",user.email,user=user)
+        mail_message("Welcome to Pitch-World","email/welcome_user", user.email,user=user)
         return redirect(url_for('auth.login'))
     return render_template('auth/signup.html',reg_form=form)
 
