3fab97e23d5cd4ea9f621e802ab33743b515aa95
Albert Byrone <albertbyrone1677@gmail.com>
2019-10-22 06:54:43 +0300

    create an update form

--- app/main/form.py
+++ app/main/form.py
@@ -1,3 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,SelectField,TextAreaField,SubmitField
 from wtforms.validators import Required
+
+class UpdateProf(FlaskForm):
+    bio = TextAreaField('Write something about you',validators=[Required()])
+    submit = SubmitField('Update')
+
